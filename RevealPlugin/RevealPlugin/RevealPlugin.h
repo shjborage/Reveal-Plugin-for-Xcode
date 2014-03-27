e1716c52581029c30926713755495c0d187e1de6
Eric <shjborage@icloud.com>
2014-03-27 16:58:26 +0800

    First Commit.
    Plugin hello world.

---
+++ RevealPlugin/RevealPlugin/RevealPlugin.h
@@ -0,0 +1,13 @@
+//
+//  RevealPlugin.h
+//  RevealPlugin
+//
+//  Created by shjborage on 3/27/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RevealPlugin : NSObject
+
+@end
---
+++ RevealPlugin/RevealPlugin/RevealPlugin.m
@@ -0,0 +1,18 @@
+//
+//  RevealPlugin.m
+//  RevealPlugin
+//
+//  Created by shjborage on 3/27/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import "RevealPlugin.h"
+
+@implementation RevealPlugin
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+  NSLog(@"pluginDidLoad");
+}
+
+@end
