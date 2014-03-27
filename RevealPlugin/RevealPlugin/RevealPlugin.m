2301a4f3285087c6a331ea50f1f579678c1de5f1
Eric <shjborage@icloud.com>
2014-03-27 17:50:22 +0800

    Add menu item and action

--- RevealPlugin/RevealPlugin/RevealPlugin.m
+++ RevealPlugin/RevealPlugin/RevealPlugin.m
@@ -10,9 +10,74 @@
 
 @implementation RevealPlugin
 
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 + (void)pluginDidLoad:(NSBundle *)plugin
 {
-  NSLog(@"pluginDidLoad");
+  NSLog(@"Reveal plugin DidLoaded");
+  
+  [self shared];
+}
+
++ (id)shared
+{
+  static dispatch_once_t onceToken;
+  static id instance = nil;
+  dispatch_once(&onceToken, ^{
+    instance = [[self alloc] init];
+  });
+  return instance;
+}
+
+- (id)init
+{
+  if (self = [super init]) {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidFinishLaunching:)
+                                                 name:NSApplicationDidFinishLaunchingNotification
+                                               object:nil];
+  }
+  return self;
+}
+
+#pragma mark - notif
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notif
+{
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(selectionDidChange:)
+                                               name:NSTextViewDidChangeSelectionNotification
+                                             object:nil];
+  
+  NSMenuItem *productMenuItem = [[NSApp mainMenu] itemWithTitle:@"Product"];
+  if (productMenuItem) {
+    NSMenu *menu = [productMenuItem submenu];
+    NSMenuItem *stopItem = [productMenuItem.submenu itemWithTitle:@"Stop"];
+    NSInteger revealIndex = [menu indexOfItem:stopItem] + 1;
+    
+//    [[productMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+    NSMenuItem *revealItem = [[NSMenuItem alloc] initWithTitle:@"Inspect with RevealApp"
+                                                        action:@selector(didPressRevealInspectMenu:)
+                                                 keyEquivalent:@""];
+    [revealItem setTarget:self];
+    [revealItem setKeyEquivalentModifierMask:NSAlternateKeyMask];
+    [[productMenuItem submenu] insertItem:revealItem atIndex:revealIndex];
+  }
+}
+
+- (void)selectionDidChange:(NSNotification *)notif
+{
+  NSLog(@"Reveal selectionDidChange:%@", notif);
+}
+
+#pragma mark - actions
+
+- (void)didPressRevealInspectMenu:(NSMenuItem *)sender
+{
+  NSLog(@"Reveal didPressRevealInspectMenu:%@", sender);
 }
 
 @end
