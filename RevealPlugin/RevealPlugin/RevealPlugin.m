27ce04036067c49ff94039ca2417efb68bf3a725
shjborage <shjborage@gmail.com>
2014-04-03 00:12:10 +0800

    fix build error with AppCode
    Coding Style is ok now.

--- RevealPlugin/RevealPlugin/RevealPlugin.m
+++ RevealPlugin/RevealPlugin/RevealPlugin.m
@@ -20,60 +20,60 @@
 + (void)pluginDidLoad:(NSBundle *)plugin
 {
   NSLog(@"Reveal plugin DidLoaded");
-  
+
   [self shared];
 }
 
 + (id)shared
 {
   static dispatch_once_t onceToken;
-    static id instance = nil;
-    dispatch_once(&onceToken, ^{
-        instance = [[self alloc] init];
-    });
-    return instance;
+  static id instance = nil;
+  dispatch_once(&onceToken, ^{
+    instance = [[self alloc] init];
+  });
+  return instance;
 }
 
 - (id)init
 {
-    if (self = [super init]) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(applicationDidFinishLaunching:)
-                                                     name:NSApplicationDidFinishLaunchingNotification
-                                                   object:nil];
+  if (self = [super init]) {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidFinishLaunching:)
+                                                 name:NSApplicationDidFinishLaunchingNotification
+                                               object:nil];
 
 //    [[NSNotificationCenter defaultCenter] addObserver:self
 //                                             selector:@selector(observeAllNotification:)
 //                                                 name:nil
 //                                               object:nil];
-    }
-    return self;
+  }
+  return self;
 }
 
 #pragma mark - notif
 
-- (void)applicationDidFinishLaunchotification *)notif
+- (void)applicationDidFinishLauning:(NSNotification *)notif
 {
 //  [[NSNotificationCenter defaultCenter] addObserver:self
 //                                           selector:@selector(selectionDidChange:)
 //                                               name:NSTextViewDidChangeSelectionNotification
 //                                             object:nil];
 
-    NSMenuItem *productMenuItem = [[NSApp mainMenu] itemWithTitle:@"Product"];
-    if (productMenuItem) {
-        NSMenu *menu = [productMenuItem submenu];
-        NSMenuItem *analyzeItem = [productMenuItem.submenu itemWithTitle:@"Analyze"];
-        NSInteger revealIndex = [menu indexOfItem:analyzeItem] + 1;
+  NSMenuItem *productMenuItem = [[NSApp mainMenu] itemWithTitle:@"Product"];
+  if (productMenuItem) {
+    NSMenu *menu = [productMenuItem submenu];
+    NSMenuItem *analyzeItem = [productMenuItem.submenu itemWithTitle:@"Analyze"];
+    NSInteger revealIndex = [menu indexOfItem:analyzeItem] + 1;
 
 //    [[productMenuItem submenu] addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *revealItem = [[NSMenuItem alloc] initWithTitle:@"Inspect with RevealApp"
-                                                            action:@selector(didPressRevealInspectProductMenu:)
-                                                     keyEquivalent:@"p"];
-        [revealItem setTarget:self];
-        [revealItem setKeyEquivalentModifierMask:NSControlKeyMask|NSCommandKeyMask];
-        [[productMenuItem submenu] insertItem:revealItem atIndex:revealIndex];
-    }ing:(NSN
-  
+    NSMenuItem *revealItem = [[NSMenuItem alloc] initWithTitle:@"Inspect with RevealApp"
+                                                        action:@selector(didPressRevealInspectProductMenu:)
+                                                 keyEquivalent:@"p"];
+    [revealItem setTarget:self];
+    [revealItem setKeyEquivalentModifierMask:NSControlKeyMask|NSCommandKeyMask];
+    [[productMenuItem submenu] insertItem:revealItem atIndex:revealIndex];
+  }
+
   NSMenuItem *debugMenuItem = [[NSApp mainMenu] itemWithTitle:@"Debug"];
   if (debugMenuItem) {
     //    [[productMenuItem submenu] addItem:[NSMenuItem separatorItem]];
@@ -109,13 +109,13 @@
 - (void)didPressRevealInspectProductMenu:(NSMenuItem *)sender
 {
   NSLog(@"Reveal didPressRevealInspectProductMenu:%@", sender);
-  
+
   NSMenuItem *productMenuItem = [[NSApp mainMenu] itemWithTitle:@"Product"];
   if (productMenuItem) {
     NSMenuItem *runItem = [productMenuItem.submenu itemWithTitle:@"Run"];
-    
+
     NSLog(@"%@ target:%@ selector:%@", runItem, runItem.target, NSStringFromSelector(runItem.action));
-    
+
     objc_msgSend(runItem.target, runItem.action);
 //    [runItem.target performSelectorOnMainThread:runItem.action withObject:runItem waitUntilDone:NO];
   }
@@ -124,7 +124,7 @@
 - (void)didPressRevealInspectDebugMenu:(NSMenuItem *)sender
 {
   NSLog(@"Reveal didPressRevealInspectDebugMenu:%@", sender);
-  
+
 }
 
 @end
