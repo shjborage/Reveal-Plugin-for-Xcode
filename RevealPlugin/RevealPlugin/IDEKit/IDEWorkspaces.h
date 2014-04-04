eeac3216627ede964f922bc3a7a26382be746b4e
shjborage <shjborage@gmail.com>
2014-04-04 17:54:40 +0800

    Revert "Revert 4bc7ff1..20ed86f"
    This reverts commit f1ae7a7e76dcd5aa720a8e22323bba6c0f9e6733.

---
+++ RevealPlugin/RevealPlugin/IDEKit/IDEWorkspaces.h
@@ -0,0 +1,38 @@
+//
+//  IDEWorkspaceWindowController.h
+//  RevealPlugin
+//
+//  Created by shjborage on 4/4/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IDENavigatorArea : NSObject
+
+@property NSArrayController *extensionsController;
+- (id)currentNavigator;
+
+@end
+
+@interface IDEWorkspaceTabController : NSObject
+
+@property (readonly) IDENavigatorArea *navigatorArea;
+@property(readonly) IDEWorkspaceTabController *structureEditWorkspaceTabController;
+
+@end
+
+@interface IDEWorkspaceWindowController : NSObject
+
+@property (readonly) IDEWorkspaceTabController *activeWorkspaceTabController;
+
+@end
+
+
+#pragma mark - Reveal Tool
+
+@interface RevealTool : NSObject
+
++ (IDEWorkspaceTabController *)workspaceControllerIn;
+
+@end
